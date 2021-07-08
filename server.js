6d0e04b16b1e091c1397d73a6debb565217b022f
Yutaka Fujii <yutakaf@playwell.site>
2021-07-08 18:15:44 +0900

    指定したディレクトリが違ったので修正

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', "static", 'index.html'));
 });
 
 app.listen(PORT,()=>{
